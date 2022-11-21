871af3bb2d13aad79242ba5195cfc94f46625bae
Sarb Bhinder <sarb8275@gmail.com>
2022-11-20 20:21:44 -0500

    Deploy on Heroku

---
+++ config/database.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+// console.log("Database String", process.env)
+mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+
+db.on("connected", ()=> {
+    console.log(`Connection Port -> ${db.port}, database -> ${db.host}`)
+})
