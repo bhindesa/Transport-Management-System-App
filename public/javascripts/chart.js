709cf01cfb25a91b41b4875b62b83b4c7a9458e5
Sarb Bhinder <sarb8275@gmail.com>
2022-11-26 04:06:25 -0500

    Adding new styling to project

--- public/javascripts/chart.js
+++ public/javascripts/chart.js
@@ -1,20 +1,20 @@
-google.charts.load('current', {'packages':['corechart']});
-google.charts.setOnLoadCallback(drawChart);
+// google.charts.load('current', {'packages':['corechart']});
+// google.charts.setOnLoadCallback(drawChart);
 
-function drawChart() {
-    var data = google.visualization.arrayToDataTable([
-      ['Country', 'Mhl'],
-      ['Fuel Consumption',2000],
-      ['Other Expense',500],
-      ['Profit',10000 - 2500],
+// function drawChart() {
+//     var data = google.visualization.arrayToDataTable([
+//       ['Country', 'Mhl'],
+//       ['Fuel Consumption',2000],
+//       ['Other Expense',500],
+//       ['Profit',10000 - 2500],
 
-    ]);
+//     ]);
 
-    var options = {
-      title:'Deliveries Report',
-      is3D:true
-    };
+//     var options = {
+//       title:'Deliveries Report',
+//       is3D:true
+//     };
 
-    var chart = new google.visualization.PieChart(document.getElementById('myChart'));
-    chart.draw(data, options);
-}
+//     var chart = new google.visualization.PieChart(document.getElementById('myChart'));
+//     chart.draw(data, options);
+// }
--- server.js
+++ server.js
@@ -3,8 +3,13 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+
+var session = require('express-session');
+var passport = require('passport');
 require('dotenv').config();
 require('./config/database');
+
+require('./config/passport');
 const methodOverride = require('method-override');
 
 
@@ -25,6 +30,15 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+//new lines of code
+app.use(session({
+  secret: 'SEIRocks!',
+  resave: false,
+  saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(methodOverride('_method'));
 
