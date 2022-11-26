13383b547de075b7009bc6429fad34b2f6255fab
Sarb Bhinder <sarb8275@gmail.com>
2022-11-26 04:13:20 -0500

    Adding new styling to project

--- server.js
+++ server.js
@@ -4,12 +4,12 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var session = require('express-session');
-var passport = require('passport');
+// var session = require('express-session');
+// var passport = require('passport');
 require('dotenv').config();
 require('./config/database');
 
-require('./config/passport');
+// require('./config/passport');
 const methodOverride = require('method-override');
 
 
@@ -31,13 +31,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 //new lines of code
-app.use(session({
-  secret: 'SEIRocks!',
-  resave: false,
-  saveUninitialized: true
-}));
-app.use(passport.initialize());
-app.use(passport.session());
+// app.use(session({
+//   secret: 'SEIRocks!',
+//   resave: false,
+//   saveUninitialized: true
+// }));
+// app.use(passport.initialize());
+// app.use(passport.session());
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(methodOverride('_method'));
